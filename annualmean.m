4f34dd393b8a13e2b9a7b77cace479d9ff706edf
Philippe Roy <borghor@yahoo.ca>
2017-05-24 10:52:55 -0400

    Optimized annualmean.m

--- annualmean.m
+++ annualmean.m
@@ -3,8 +3,8 @@ function indicator = annualmean(data, var)
   
   dates = data.dates;
   
-  min_year = min(unique(dates(:, 1)));
-  max_year = max(unique(dates(:, 1)));
+  min_year = min(dates(:, 1));
+  max_year = max(dates(:, 1));
   fields = fieldnames(data);
   time_dim = (size(data.(fields{1})) == size(dates,1));
   indicator.data  = zeros(size(min_year:max_year, 2), size(data.(fields{1}), find(~time_dim)));
@@ -16,8 +16,7 @@ function indicator = annualmean(data, var)
       error(strcat('this indicator requires a data struct with .', var))
   end
   
-  for y = min_year:max_year
-    
+  for y = min_year:max_year    
     indicator.data(y-min_year +1, :) = squeeze(mean(F(dates(:, 1) == y, :), 1));
     indicator.dates(y - min_year +1, :) = y;
   end

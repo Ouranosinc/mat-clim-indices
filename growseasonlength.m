25821ed8c54f4075740d246a7ae506ac4fb9869d
Philippe Roy <borghor@yahoo.ca>
2017-05-24 10:47:37 -0400

    optimized growseasonlength.m

--- growseasonlength.m
+++ growseasonlength.m
@@ -34,44 +34,42 @@ t55 = 5.5; % Threshold for start and end of growing season
 W=[ 1 4 6 4 1 ] ./ 16; % Weights for moving average
 
 for y = min_year:max_year
+    
     temp = data.tas(dates(:, 1) == y, :);
     temp_dates = datenum(dates(dates(:,1) == y, :));
     days = unique(floor(temp_dates));
     
-    for d = 1:length(days)
-        temp_d = temp(floor(temp_dates) == days(d), :);%this is to remove feb 29 and 30
-        day_temp(d, :) = temp_d(end,:);
-        day_dates(d, :) = floor(temp_dates(1)) + d - 1;
-    end
-    
+    temp2 = NaN(length(3:length(days) - 2), size(temp, 2));
+
     for d = 3:length(days) - 2    % center run avg, no values for 1st and last 2
-        temp2(d-3+1, :) = W * day_temp(d-2:d+2,:);%look out, matrix multiply
+        temp2(d-3+1, :) = W * temp(d-2:d+2,:); % look out, matrix multiply
     end
     log   = temp2 >= t55;
     cum_  = cumsum(log);
     cum_2 = (cum_(1+5:end, :) - cum_(1:end-5, :)) == 5; % find where run avg is 5.5 for 5 days
     first = arrayfun(@(i) find(cum_2(:, i) ~= 0, 1, 'first'), 1:size(cum_2, 2), 'UniformOutput', false);
     last = arrayfun(@(i) find(log(:, i) ~= 0, 1, 'last'), 1:size(log, 2), 'UniformOutput', false);
+    
     for i=1:size(cum_2,2)
-        %I want the 5th day of the series of 5,
-        if code == 1
+        
+        if code == 1 % start of growing season length
             try
-                indicator.data(y - min_year +1,i) = day_dates(first{i} + 2 + 5 + 1, :) - datenum([y 1 1]);
+                indicator.data(y - min_year +1,i) = days(first{i} + 2 + 5 + 1, :) - datenum([y 1 1]);
             catch
                 indicator.data(y - min_year + 1, i) = 0;
             end
             
-        elseif code == 2
+        elseif code == 2 % end of growing season length
             try
-                indicator.data(y - min_year + 1, i) = day_dates(last{i} + 2, :) - datenum([y 1 1]);
+                indicator.data(y - min_year + 1, i) = days(last{i} + 2, :) - datenum([y 1 1]);
             catch
                 indicator.data(y - min_year + 1, i) = 0;
             end
             
-        elseif code == 3
+        elseif code == 3 % length of growing season length
             try
-                indicator.data(y - min_year + 1, i)=day_dates(last{i} + 2, :) - ...
-                    day_dates(first{i} + 2 + 5 + 1, :);
+                indicator.data(y - min_year + 1, i) = days(last{i} + 2, :) - ...
+                    days(first{i} + 2 + 5 + 1, :);
             catch
                 indicator.data(y - min_year + 1, i) = 0;
             end

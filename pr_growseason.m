cdf51ea115989bdae1802e122829525e3201fe8c
Philippe Roy <borghor@yahoo.ca>
2017-05-31 09:50:15 -0400

    Added title to output struct

--- pr_growseason.m
+++ pr_growseason.m
@@ -34,6 +34,7 @@ for y = min_year:max_year
     indicator.dates(y-min_year +1,:) = [y 8 1];
 end
 
-indicator.units = 'mean precip during growing season (mm)';
+indicator.units = 'Precip during growing season (mm)';
+indicator.title = 'Precipitation total during growing season (mm)';
 indicator.dates(y - min_year + 1, :) = [y 8 1];
 end

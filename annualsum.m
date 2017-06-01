cb2877203f5b34cd8cd8be6bb359e372cd31e5d7
Philippe Roy <borghor@yahoo.ca>
2017-06-01 15:53:21 -0400

    Corrected metadata output bug

--- annualsum.m
+++ annualsum.m
@@ -17,11 +17,11 @@ function indicator = annualsum(data, var)
   end
   
   for y = min_year:max_year    
-    indicator.data(y-min_year +1, :) = squeeze(sum(F(dates(:, 1) == y, :)));
+    indicator.data(y - min_year + 1, :) = squeeze(sum(F(dates(:, 1) == y, :)));
     indicator.dates(y - min_year +1, :) = y;
   end
   
   indicator.units = 'deg C';
-  indicator.var = strcat(['Annual max of ', var]);
+  indicator.var = strcat(['Annual sum of ', var]);
   indicator.title = strcat(['Annual sum of ', var]);
 end
